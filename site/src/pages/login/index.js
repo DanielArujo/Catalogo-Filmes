a54927aea46c121e8e5e1ee6815084e242883cdd
Daniel Arujo <daniel.araujo@gmail.com>
2022-07-31 20:08:08 -0300

    loginn

--- site/src/pages/login/index.js
+++ site/src/pages/login/index.js
@@ -1,6 +1,18 @@
+import { useState } from 'react'
+import axios from 'axios'
 import './index.scss'
 
 export default function Index() {
+
+    [email, setEmail] = useState('');
+    [senha, setSenha] = useState('');
+
+
+    function entrarLogin(){
+        
+    }
+
+
     return (
         <main className='page page-login'>
             <section className="box-login">
@@ -13,14 +25,14 @@ export default function Index() {
                 <div>
                     <div className='form-row'>
                         <label>E-mail:</label>
-                        <input type='text' placeholder='Informe seu e-mail' />
+                        <input type='text' placeholder='Informe seu e-mail' value={email} onChange={ e => setEmail (e.target.value) } />
                     </div>
                     <div className='form-row'>
                         <label>Senha:</label>
-                        <input type='password' placeholder='***' />
+                        <input type='password' placeholder='***' value={senha} onChange={ e => setSenha (e.target.value) } />
                     </div>
                     <div className='form-entrar'>
-                        <button className='btn-black'>ENTRAR</button> 
+                        <button className='btn-black' onClick={ entrarLogin }>ENTRAR</button> 
                     </div>
                     <div className='form-entrar invalido'>
                         Credenciais inválidas
